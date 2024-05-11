c6b2d02795d32c6191652d41b9ed039ffb0e74de
UmerThePassionate <umersaeed0234567890@gmail.com>
2024-05-11 17:55:23 +0500

    add

---
+++ 13-Switch Statement.php
@@ -0,0 +1,89 @@
+<?php
+
+// Switch Statement
+
+//  Syntax 
+
+// method 1
+
+// switch (expression){
+//     case condition 1:
+//            statement
+//         break;
+//     case condition 2:
+//            statement
+//         break;
+//     case condition 3:
+//            statement
+//         break;
+//     default:
+//            statement
+//         break;
+// }
+
+// method 2
+
+// switch (expression): 
+    // case condition 1:
+        // statement
+    // case condition 2:    
+        // statement
+    // case condition 3:
+        // statement
+    // default: 
+    //     statement
+// endswitch;   
+
+
+// example
+
+// $weekday = 0;
+
+// switch($weekday){
+//     case 1:
+//         echo "Today is monday";
+//         break;
+//     case 2:
+//         echo "Today is tuesday";
+//         break;
+//     case 3: 
+//         echo "Today is  wednesday";
+//         break;
+//     case 4:
+//         echo "Today is thursday";
+//         break;
+//     case 5;
+//         echo "Today is friday";
+//         break;
+//     case 6:
+//         echo "Today is saturday";
+//         break;
+//     case 7:
+//         echo "Today is sunday";
+//         break;
+//     default:    
+//         echo "Invalid day";
+// }
+
+// example 2
+
+// $age = 28;
+
+// switch(true){
+//     case ($age >= 15 && $age <=20):
+//         echo "you are eligible";
+//         break;
+//     case ($age >= 21 && $age <= 30):
+//         echo "you are not eligible";
+//         break;
+//     default:    
+//         echo "Invalid age";
+// }
+
+
+
+
+
+
+
+?>
---
+++ 14-Ternary Operator.php
@@ -0,0 +1,25 @@
+<?php
+
+// Ternary Operators
+
+// Syntax
+
+// (condition) ? true statment: false statment
+
+// example 1
+
+// $x =  21;
+
+// ($x > 20)? $z  = "greater than 20" : $z = "smaller than 20";
+
+// echo $z;
+
+// example 2
+
+// $x =  21;
+
+// $z ="value is :". ($x > 20? " greater than 20" : "smaller than 20");
+
+// echo $z;
+
+?>
