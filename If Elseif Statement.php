de645c48a3b1e4471d7dde3d62cdf654774a37f9
UmerThePassionate <umersaeed0234567890@gmail.com>
2024-05-11 15:22:10 +0500

    Create If Elseif Statement.php

---
+++ If Elseif Statement.php
@@ -0,0 +1,67 @@
+<?php
+
+// If elseif statement
+
+// syntax
+
+// method 1
+
+// if(conndition 1){
+//     statement 1
+// } else if(conndition 2){
+//     statement 2
+// }else{
+//     default statement
+// }
+
+// method 2
+
+// if(conndition 1):    
+    // statement 1
+// elseif(conndition 2):    
+    // statement 2
+// else:    
+    // default statement
+// endif;   
+
+// Example
+
+// first method
+
+// $per = 90;
+
+// if($per <= 100 && $per >= 80){
+//     echo "You are in Merit";
+// }elseif($per <= 79 && $per >= 60){
+//     echo "You are in 1st devision";
+// }elseif($per <= 59 && $per >= 45){
+//     echo "You are in 2nd devision";    
+// }elseif($per <= 44 && $per >=  33){
+//     echo "You are in 3rd devision";    
+// }elseif($per < 33  && $per = 0){
+//     echo "You are Fail";
+// }else{
+//     echo "Invalid Percentage";
+// }
+
+// second method
+
+// $per = -1;
+
+// if($per <= 100 && $per >= 80):   
+//     echo "You are in Merit";
+// elseif($per <= 79 && $per >= 60):    
+//     echo "You are in 1st devision";
+// elseif($per <= 59 && $per >= 45):
+//     echo "You are in 2nd devision";    
+// elseif($per <= 44 && $per >= 33):    
+//     echo "You are in 3rd devision";    
+// elseif($per < 33  && $per == 0):
+//     echo "You are Fail";
+// else:  
+//     echo "Invalid Percentage";
+// endif;    
+
+
+
+?>
